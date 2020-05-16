1207410e6b6cb1a01ed56c93833d4604d0829d1a
Grigorii Mironov <gmironow@gmail.com>
2020-05-16 16:16:59 +0300

    Scan Decoder updated

--- scanner/src/main/java/com/finnflare/scanner/ScanDecoder.kt
+++ scanner/src/main/java/com/finnflare/scanner/ScanDecoder.kt
@@ -10,25 +10,34 @@ object ScanDecoder {
 
         if (Pattern.matches("^\\d{13}$", scanRes)){
             gtin = scanRes
+
+            return Triple(gtin, sn, rfid)
         }
 
-        if (Pattern.matches("^.?01\\d{14}21.{13}", scanRes)){
+        if (Pattern.matches("^.?01\\d{14}21.{13}" + 29.toChar() + "?.*$", scanRes)){
             val startIndex: Int = scanRes.indexOf("01") + 2
 
             gtin = scanRes.substring(startIndex, startIndex + 14)
             sn = scanRes.substring(startIndex + 16, startIndex + 29)
+
+            return Triple(gtin, sn, rfid)
         }
 
-        if (Pattern.matches("^.{18}49BD1A$", scanRes) && scanRes.endsWith("48BD1A") ){
+        if (Pattern.matches("^.{18}49BD1A$", scanRes)){
             val gtinEncoded = scanRes.substring(0, 10).toLong(16)
             gtin = (gtinEncoded.toString() + controlNumberGTIN(gtinEncoded.toString()))
             rfid = scanRes
+
+            return Triple(gtin, sn, rfid)
         }
 
-        if (Pattern.matches("^.{20}49BD$", scanRes) && scanRes.endsWith("48BD1A") ){
+        if (Pattern.matches("^.{20}49BD$", scanRes)){
             gtin = scanRes.substring(0, 12).toLong(16).toString()
-            sn = scanRes.substring(12, 20)
+            sn = scanRes.substring(12, 20).toLong(16).toString()
+            sn = sn.padStart(12 - sn.length, '0')
             rfid = scanRes
+
+            return Triple(gtin, sn, rfid)
         }
 
         return Triple(gtin, sn, rfid)
