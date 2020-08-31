3205e6f23cc2edf859a4f9f4145eb678616b98ad
Gregory <devgregorymironov@gmail.com>
2020-08-31 14:55:26 +0300

    App logo updated

--- app/src/main/java/com/finnflare/android_dct/ui/MainActivity.kt
+++ app/src/main/java/com/finnflare/android_dct/ui/MainActivity.kt
@@ -3,16 +3,17 @@ package com.finnflare.android_dct.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.finnflare.android_dct.R
 import com.finnflare.android_dct.ui.login.LoginActivity
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 
 class MainActivity : AppCompatActivity() {
 
+    @ObsoleteCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+        finish()
     }
 }
